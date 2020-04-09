e87b12fd41111128f32f071765a659e8ff2341c6
Thierry Treyer <zehkae@gmail.com>
2020-04-10 01:44:33 +0200

    Start CharacterAirControl

---
+++ Assets/Scripts/CharacterAirControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterAirControl : MonoBehaviour {
+    public float maxSpeed = 5f;
+
+    private Rigidbody2D rbody;
+
+    private float movement;
+    private bool jump;
+
+    public void OnMovement(InputValue value) {
+        movement = value.Get<float>();
+    }
+
+    public void OnJump(InputValue value) {
+        Debug.Log(value.Get<float>());
+    }
+
+    private void Awake() {
+        //rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable() {
+        rbody.gravityScale = 1;
+    }
+
+}
