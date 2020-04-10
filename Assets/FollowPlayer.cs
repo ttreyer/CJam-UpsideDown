0dea5db7e0433a56bbd4e8cbab7a91660e1a3fbd
Thierry Treyer <zehkae@gmail.com>
2020-04-10 17:24:52 +0200

    Auto setup for camera

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class FollowPlayer : MonoBehaviour {
+    private void Awake() =>
+        GetComponent<CinemachineVirtualCamera>().Follow = GameObject.FindGameObjectWithTag("Player").transform;
+}
