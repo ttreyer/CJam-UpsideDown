7da2228b1bd3c9bc6d5e8ab1d51aa817c0f30edc
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-04-10 18:58:33 +0200

    Add randomTorque in entry of water

---
+++ Assets/Scripts/Props/RandomTorque.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTorque : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void ApplyRotation()
+    {
+        int direction = (Random.Range(0, 2) > 0) ? 1 : -1;
+        _rb.AddTorque(direction * Random.Range(5f, 15f));
+    }
+}
