edd8e516f0188e0003fe1dee7b9e735b683d4134
pete842 <pierrekohler94@gmail.com>
2020-04-10 13:00:51 +0200

    Add DOTween + room pitchTo

--- Assets/Scripts/Character/CharacterWaterControl.cs
+++ Assets/Scripts/Character/CharacterWaterControl.cs
@@ -4,48 +4,48 @@ using UnityEngine.InputSystem;
 
 public class CharacterWaterControl : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float waterGravityScaleSwim = 0.5f;
     [SerializeField] private float swimVelocity = 3.0f;
     [SerializeField] private float drownVelocity = 2.0f;
     [SerializeField] private float sideVelocity = 3.0f;
 
+    private Rigidbody2D _rb;
     private bool _swim = false;
     private bool _isSwimming = false;
     private float _horizontalAxis = 0;
 
-    private void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable()
     {
-        rb.gravityScale = 0;
+        _rb.gravityScale = 0;
     }
 
     void FixedUpdate()
     {
         if (_swim)
         {
-            rb.velocity = Vector2.up * swimVelocity;
+            _rb.velocity = Vector2.up * swimVelocity;
             _isSwimming = true;
             _swim = false;
 
-            rb.gravityScale = waterGravityScaleSwim;
+            _rb.gravityScale = waterGravityScaleSwim;
         }
-        else if (_isSwimming && rb.velocity.y < drownVelocity / -2.0f)
+        else if (_isSwimming && _rb.velocity.y < drownVelocity / -2.0f)
         {
             _isSwimming = false;
-            rb.gravityScale = .0f;
+            _rb.gravityScale = .0f;
         }
         else if (!_isSwimming)
         {
-            rb.velocity = Vector2.down * drownVelocity;
+            _rb.velocity = Vector2.down * drownVelocity;
         }
 
 
-        rb.velocity = new Vector2(_horizontalAxis * sideVelocity, rb.velocity.y);
+        _rb.velocity = new Vector2(_horizontalAxis * sideVelocity, _rb.velocity.y);
     }
 
     private void OnMovement(InputValue value)
--- Assets/Scripts/Level/RoomRobin.cs
+++ Assets/Scripts/Level/RoomRobin.cs
@@ -1,19 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class RoomRobin : MonoBehaviour
 {
-    private float _velocity = 2.0f;
+    [SerializeField] private float basicVelocity = 2.0f;
+
+    private bool _isPitching;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.eulerAngles = new Vector3(0, 0,  transform.eulerAngles.z + _velocity * Time.fixedDeltaTime);
+        if (_isPitching)
+            return;
+
+        transform.eulerAngles += new Vector3(0, 0, basicVelocity * Time.fixedDeltaTime);
     }
 
-    public void SetVelocity(float velocity)
+    public void PitchTo(float targetAngle, float duration = 5.0f)
     {
-        _velocity = velocity;
+        if (_isPitching)
+            return;
+
+        var angles = transform.eulerAngles;
+        _isPitching = true;
+        transform.DORotate(new Vector3(angles.x, angles.y, targetAngle), duration).SetEase(Ease.InOutElastic).OnComplete(ToggleIsPitching);
     }
+
+    private void ToggleIsPitching() => _isPitching = !_isPitching;
 }
