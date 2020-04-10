1e639ff3311af2690396bdfc9636e4513fbd12c9
Filipe Fortunato <f.filipe.30.03@gmail.com>
2020-04-10 18:59:27 +0200

    Add missing filed, thanks git GUI

---
+++ Assets/Scripts/Level/LoadNextLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int nextSceneID;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+       if(collision.gameObject.CompareTag("Finish"))
+       {
+           SceneManager.LoadScene (nextSceneID);
+       }
+    }
+}
