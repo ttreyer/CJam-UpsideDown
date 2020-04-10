080584bba6b3ee93a798d7f728d29049f4053ef3
Thierry Treyer <zehkae@gmail.com>
2020-04-10 16:07:02 +0200

    Neat Buyancy

---
+++ Assets/Scripts/Buyancy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buyancy : MonoBehaviour {
+    public float magnitude = 10f;
+    private Rigidbody2D rbody;
+
+    private void Awake() {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable() {
+        rbody.velocity /= 2f;
+        rbody.drag = 5f;
+        rbody.angularDrag = 5f;
+    }
+
+    private void OnDisable() {
+        rbody.drag = 0f;
+        rbody.angularDrag = 0.05f;
+    }
+
+    private void FixedUpdate() =>
+        rbody.AddForce(Vector2.up * magnitude);
+}
---
+++ Assets/Scripts/WaterToggle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WaterToggle : MonoBehaviour {
+    public UnityEvent onWaterEnter;
+    public UnityEvent onWaterExit;
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.name == "Water")
+            onWaterEnter.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.name == "Water")
+            onWaterExit.Invoke();
+    }
+}
