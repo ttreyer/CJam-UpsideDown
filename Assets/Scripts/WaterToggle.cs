71e6d49450f60860114a4b6a5cc0249115291cb1
Thierry Treyer <zehkae@gmail.com>
2020-04-10 16:13:31 +0200

    Flex Water

--- Assets/Scripts/WaterToggle.cs
+++ Assets/Scripts/WaterToggle.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class WaterToggle : MonoBehaviour {
+    public LayerMask whatIsWater;
     public UnityEvent onWaterEnter;
     public UnityEvent onWaterExit;
 
+    private bool IsWater(GameObject other) =>
+        (whatIsWater.value | other.layer) != 0;
+
     private void OnTriggerEnter2D(Collider2D collision) {
-        if (collision.name == "Water")
+        if (IsWater(collision.gameObject))
             onWaterEnter.Invoke();
     }
 
     private void OnTriggerExit2D(Collider2D collision) {
-        if (collision.name == "Water")
+        if (IsWater(collision.gameObject))
             onWaterExit.Invoke();
     }
 }
