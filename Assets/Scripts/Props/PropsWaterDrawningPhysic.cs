e75c017d4c344d309f4eed072dc0fa0f6dcc5f4e
pete842 <pierrekohler94@gmail.com>
2020-04-10 16:46:48 +0200

    Merge Controls

---
+++ Assets/Scripts/Props/PropsWaterDrawningPhysic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class PropsWaterDrawningPhysic : MonoBehaviour
+{
+    [SerializeField] private float gravityScale = .75f;
+    private Rigidbody2D _rb;
+    private float _velocity;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // _rb.velocity = new Vector2(_rb.velocity.x, _velocity);
+    }
+
+    private void OnEnable()
+    {
+        //_velocity = Math.Min(_rb.velocity.y / 2.0f, maxVelocity);
+        _rb.drag = 30;
+        _rb.gravityScale = gravityScale;
+    }
+}
