db35400a78cf3cc818c426e53f0ced24635f6654
pete842 <pierrekohler94@gmail.com>
2020-04-10 18:52:20 +0200

    Damage detection first shot

---
+++ Assets/Scripts/Character/CharacterDamagesDetection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDamagesDetection : MonoBehaviour
+{
+    [SerializeField] private float deadlyForce = 10.0f;
+    
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Deadly Item"))
+        {
+            var impact = (other.rigidbody.mass * other.relativeVelocity).magnitude;
+
+            Debug.Log("impact: " + impact);
+
+            if (impact >= deadlyForce)
+            {
+                Debug.Log("You're dead!");
+            }
+        }
+    }
+}
