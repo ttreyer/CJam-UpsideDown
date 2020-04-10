d779070f33679cd84ace53d13c8020da9c64c9bf
Thierry Treyer <zehkae@gmail.com>
2020-04-10 02:18:27 +0200

    Detect ground

--- Assets/Scripts/CharacterAirControl.cs
+++ Assets/Scripts/CharacterAirControl.cs
@@ -5,26 +5,47 @@ using UnityEngine.InputSystem;
 
 public class CharacterAirControl : MonoBehaviour {
     public float maxSpeed = 5f;
+    public float jumpVelocity = 20f;
+
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    private bool isGrounded;
 
     private Rigidbody2D rbody;
 
     private float movement;
     private bool jump;
 
-    public void OnMovement(InputValue value) {
+    /* Input handling */
+    private void OnMovement(InputValue value) =>
         movement = value.Get<float>();
-    }
 
-    public void OnJump(InputValue value) {
-        Debug.Log(value.Get<float>());
-    }
+    private void OnJump (InputValue value) =>
+        jump = value.Get<float>() > 0f;
 
     private void Awake() {
-        //rbody = GetComponent<Rigidbody2D>();
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     private void OnEnable() {
         rbody.gravityScale = 1;
     }
 
+    private bool CanJump() {
+        return isGrounded;
+    }
+
+    private void Update() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void FixedUpdate() {
+        var velocity = new Vector2(movement * maxSpeed, rbody.velocity.y);
+
+        if (jump && CanJump())
+            velocity.y = jumpVelocity;
+
+        rbody.velocity = velocity;
+    }
 }
