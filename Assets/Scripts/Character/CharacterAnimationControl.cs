7e49fe879bc492d0aeca2b8fc5e7edae7b174305
pete842 <pierrekohler94@gmail.com>
2020-04-11 13:24:34 +0200

    better water character animation

--- Assets/Scripts/Character/CharacterAnimationControl.cs
+++ Assets/Scripts/Character/CharacterAnimationControl.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 public class CharacterAnimationControl : MonoBehaviour
 {
     [SerializeField] public bool isInWater = false;
-    
+
     [SerializeField] private LayerMask whatIsGround;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private float groundCheckRadius = 0.5f;
@@ -22,36 +22,41 @@ public class CharacterAnimationControl : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
     }
-    
+
     /* Input handling */
     private void OnMovement(InputValue value) =>
-        _animator.SetFloat("velocity.x", Math.Abs(value.Get<float>()));
+        _inputMovement = value.Get<float>();
 
     private void OnJump(InputValue value) =>
-        _animator.SetBool("isJumping", value.Get<float>() > .0f);
+        _jump = value.Get<float>() > 0f;
 
     private void FixedUpdate()
     {
         _isGrounded = Physics2D.Raycast(groundCheck.position, Vector2.down, groundCheckRadius, whatIsGround);
+        
+        _animator.SetFloat("velocity.x", Math.Abs(_inputMovement));
+        _animator.SetBool("isJumping", _jump);
+        _animator.SetFloat("velocity.y", _rb.velocity.y);
+        _animator.SetBool("isGrounded", _isGrounded);
+        _animator.SetBool("isInWater", isInWater);
+    }
 
+    private void Update()
+    { 
         if (_rb.velocity.x < -0.3f)
         {
-            if(!_flip)
+            if (!_flip)
                 transform.Rotate(Vector3.up, 180);
-            
+
             _flip = true;
         }
         else if (_rb.velocity.x > 0.3f)
         {
-            if(_flip)
+            if (_flip)
                 transform.Rotate(Vector3.up, 180);
-            
+
             _flip = false;
         }
-        
-        _animator.SetFloat("velocity.y", _rb.velocity.y);
-        _animator.SetBool("isGrounded", _isGrounded);
-        _animator.SetBool("isInWater", isInWater);
     }
 
     public void IsInWater(bool itIs) => isInWater = itIs;
