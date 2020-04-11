2caf6637d6505df0f4a7d8d46ccbb35e897f9425
Jonathan Zaehringer <jonathan.zaehringer@gmail.com>
2020-04-11 17:51:20 +0200

    Add Title screen with starting game

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    private void OnJump(InputValue value) {
+        SceneManager.LoadScene((SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+    }
+}
