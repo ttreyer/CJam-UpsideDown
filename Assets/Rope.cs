bab46a4c348d70826fc98d671eb1bf9ee5d6bdc5
Thierry Treyer <zehkae@gmail.com>
2020-04-15 01:31:36 +0200

    Improve Rope

--- Assets/Rope.cs
+++ Assets/Rope.cs
@@ -4,34 +4,40 @@ using UnityEngine;
 
 public class Rope : MonoBehaviour {
     public GameObject linkPrefab;
-    public Rigidbody2D startAnchor, endAnchor;
-    public Transform anch;
+
+    public Rigidbody2D startObject ;
+    public Transform startAnchor;
+
+    public Rigidbody2D endObject;
+    public Transform endAnchor;
+
     public int linksCount = 7;
 
     private LineRenderer lineRenderer;
     private List<GameObject> segments = new List<GameObject>();
 
     private void GenerateRope() {
-        segments.Add(startAnchor.gameObject);
-        Rigidbody2D parent = startAnchor;
-        for (int i = 0; i < linksCount; ++i) {
-            GameObject link = Instantiate(linkPrefab, transform);
-            link.transform.position = startAnchor.position + i * new Vector2(0, -.475f);
-            HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
-            joint.connectedBody = parent;
+        Rigidbody2D parent = startObject;
+        segments.Add(parent.gameObject);
 
-            segments.Add(link);
-            parent = link.GetComponent<Rigidbody2D>();
+        if (linksCount > 1) {
+            for (int i = 0; i < linksCount - 1; ++i) {
+                GameObject link = Instantiate(linkPrefab, transform);
+                link.transform.position = parent.position + new Vector2(0, -.475f);
+                HingeJoint2D joint = link.GetComponent<HingeJoint2D>();
+                joint.connectedBody = parent;
+
+                segments.Add(link);
+                parent = link.GetComponent<Rigidbody2D>();
+            }
         }
 
-        if (endAnchor) {
-            HingeJoint2D lastLink = endAnchor.gameObject.AddComponent<HingeJoint2D>();
-            lastLink.connectedBody = parent;
-            lastLink.autoConfigureConnectedAnchor = false;
-            lastLink.anchor = anch.localPosition;
-            lastLink.connectedAnchor = new Vector2(0f, -0.3f);
-            segments.Add(anch.gameObject);
-        }
+        HingeJoint2D lastLink = endObject.gameObject.AddComponent<HingeJoint2D>();
+        lastLink.connectedBody = parent;
+        lastLink.autoConfigureConnectedAnchor = false;
+        lastLink.anchor = endAnchor.localPosition;
+        lastLink.connectedAnchor = new Vector2(0f, -0.3f);
+        segments.Add(endAnchor.gameObject);
     }
 
     private void DrawRope() {
